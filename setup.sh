ff2d2c07e8bd45da6c87d745fcea605ce84aae9f
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-13 10:45:44 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git clone https://github.com/DPGA/Analyse
+git clone https://github.com/DPGA/Daq
+git clone https://github.com/DPGA/Lib
+git clone https://github.com/DPGA/Monitoring
+git clone https://github.com/DPGA/QtDcs

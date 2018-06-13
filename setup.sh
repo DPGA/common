881ad0e8f7c033f3e42b13d5a7e76e03ecad8c0f
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-13 11:05:06 +0200

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/DPGA/Analyse
 git clone https://github.com/DPGA/Daq
 git clone https://github.com/DPGA/Lib
 git clone https://github.com/DPGA/Monitoring
-git clone https://github.com/DPGA/QtDcs
+git clone https://github.com/DPGA/Dcs
 mkdir build
 cmake ../common/.
 make

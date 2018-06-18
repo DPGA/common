0de42af910ccee475793ba52565b1b0e4a8bcea2
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-18 16:38:45 +0200

    Add git libqwt source  to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 git clone https://github.com/ntop/PF_RING.git Pf_Ring
+wget https://sourceforge.net/projects/qwt/files/qwt/6.1.3/qwt-6.1.3.tar.bz2
+tar vjxf qwt-6.1.3.tar.bz2 Lib/.
+git clone https://github.com/DPGA/ConfigDcs
 git clone https://github.com/DPGA/Analyse
 git clone https://github.com/DPGA/Daq
 git clone https://github.com/DPGA/Lib

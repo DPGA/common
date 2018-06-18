5403e5da3ec952bd1e8cca82a944c98e20ae3bf1
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-18 16:32:54 +0200

    Add git pf_ring to setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git clone https://github.com/ntop/PF_RING.git Pf_Ring
 git clone https://github.com/DPGA/Analyse
 git clone https://github.com/DPGA/Daq
 git clone https://github.com/DPGA/Lib

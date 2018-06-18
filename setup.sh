aed15151ae158f52056623dc1b34361159fc2896
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-18 16:45:00 +0200

    Add deplace wget libqwt  to setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 git clone https://github.com/ntop/PF_RING.git Pf_Ring
-wget https://sourceforge.net/projects/qwt/files/qwt/6.1.3/qwt-6.1.3.tar.bz2; cd Lib;tar vjxf qwt-6.1.3.tar.bz2 ;cd ..
+
 git clone https://github.com/DPGA/ConfigDcs
 git clone https://github.com/DPGA/Analyse
 git clone https://github.com/DPGA/Daq
 git clone https://github.com/DPGA/Lib
 git clone https://github.com/DPGA/Monitoring
 git clone https://github.com/DPGA/Dcs
+wget https://sourceforge.net/projects/qwt/files/qwt/6.1.3/qwt-6.1.3.tar.bz2; cd Lib;tar vjxf qwt-6.1.3.tar.bz2 ;cd ..
 mkdir build
 cd build
 cmake ../common/.
